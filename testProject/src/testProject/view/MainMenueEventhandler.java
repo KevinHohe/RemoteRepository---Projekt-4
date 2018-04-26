ca7a2c58eea0752eab4bf1f062b2331a91d21169
David Lippert <david@lippert-familie.de>
2018-04-26 13:40:04 +0200

    Packagestruktur geändert

---
+++ testProject/src/testProject/view/MainMenueEventhandler.java
@@ -0,0 +1,25 @@
+/*
+ * Project     : testProject
+ * Classname   : MainMenueEventhandler
+ * Version     : 1
+ * Date        : 26.04.2018 13:31:13
+ * Author      : <a href="mailto:Teilnehmer@soka-bau.de">Teilnehmer</a>
+ * Copyright(c): SOKA-BAU 2018
+ */
+package testProject.view;
+
+/**
+ * Koordiiert den Aufbau der
+ * @author <a href="mailto:david@lippert-familie.de">David Lippert</a>
+ * @since 26.04.2018 13:38:47 <a href="mailto:david@lippert-familie.de">David Lippert</a> | Erstellt
+ */
+public class MainMenueEventhandler {
+  ///* ---- Konstante ------------------------------------------------------------ */
+  ///* ---- Attribute ------------------------------------------------------------ */
+  ///* ---- Start ---------------------------------------------------------------- */
+  ///* ---- Konstruktor ---------------------------------------------------------- */
+  ///* ---- Initialisierung ------------------------------------------------------ */
+  ///* ---- Logik ---------------------------------------------------------------- */
+  ///* ---- get/is/set/add ------------------------------------------------------- */
+  ///* ---- create --------------------------------------------------------------- */
+}
