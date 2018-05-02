ef5390e299f6a0e4b1b7a70b36eedbd170668556
Kevin Hohe <kevin.hohe@ibs-banking.com>
2018-05-02 10:57:34 +0200

    Updated gitignore, removed .project and .classpath from cache

--- testProject/src/testProject/view/AddCardsEventhandler.java
+++ testProject/src/testProject/view/AddCardsEventhandler.java
@@ -3,7 +3,6 @@ package testProject.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class AddCardsEventhandler {
